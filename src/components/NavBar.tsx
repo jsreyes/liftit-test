19cf5c1576f286c31ee4a6a460cc727cc8896219
Javier Reyes <jreyes@portalfinance.co>
2019-03-05 20:26:03 -0500

    Refactor navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,29 @@
-import * as React from 'react'
+import * as React from 'react';
+import { Link } from 'react-router-dom'
 
-export default class NavBar extends React.Component{
+import { faMapMarkedAlt,faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+// definiendo estilos para Button
+const style = {
+ link: {
+  color: '#555',
+  textDecoration: 'none'
+ },
+ navbar: {
+  borderBottom: 'solid 1px #aaa',
+  padding: '10px 15px',
+ },
+}
+
+export default class NavBar extends React.Component {
  public render() {
   return (
-   <div>
-    este es el footer
+   <div style={style.navbar}>
+    <Link style={style.link} to='/app/newsfeed'><FontAwesomeIcon icon={faMapMarkedAlt} /> Liftit Test</Link>
+    <div style={{ float: 'right' }}>
+     <Link style={style.link} to='/app/profile'><FontAwesomeIcon icon={faSignOutAlt} /> Salir</Link>
+    </div>
    </div>
   )
  }
